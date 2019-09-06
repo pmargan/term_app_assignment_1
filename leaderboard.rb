c967285379ac48022ff8723a135e53bf55824356
pmargan <pmargan@gmail.com>
2019-09-06 10:01:53 +1000

    added comments for leaderboard module

--- leaderboard.rb
+++ leaderboard.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
-require 'artii'
+require 'colorize'
 # Leaderboard module
 module Leaderboard
   def leaderboard
+    # Reads into a hash from leaderboard file
     h = Hash[*File.read('./leaderboard').split(/[, \n]+/)]
+    # Sorts by value, then reverses it and takes top 5
     myhash = Hash[h.sort_by { |_k, v| v }.reverse].first(5)
+    # Outputs the results to screen
     puts 'TOP 5 SCORES ON LEADERBOARD'.colorize(:green)
     puts myhash
   end
